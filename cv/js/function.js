cdda89b54cc7041bbb01933f85d4b9ea6a225e87
Magui0806 <125514776+Magui0806@users.noreply.github.com>
2023-02-25 13:04:57 -0300

    Add files via upload

---
+++ cv/js/function.js
@@ -0,0 +1,24 @@
+document.getElementById("botoncruz").addEventListener("click", function(){
+    var x = document.getElementById("certcruz");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  });
+  document.getElementById("bio").addEventListener("click", function(){
+    var x = document.getElementById("bioqui");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  });
+  document.getElementById("prog").addEventListener("click", function(){
+    var x = document.getElementById("cert prog ferro");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  });
